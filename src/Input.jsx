0fe72d4b5a2a1574bacb4d74d744b9cb761d818c
Adarsh Gaur <adarshgaur17@gmail.com>
2022-09-26 14:48:18 +0530

    add input component into login page

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import { useField } from "formik";
+import React from "react";
+import { GrEmergency } from "react-icons/gr";
 
-function Input(props) {
-  const [value, setValue] = useState(props.dValue);
+function Input({ label, id, name, ...rest }) {
+  const field = useField(name);
+  const [data, meta] = field;
+  const { value, onBlur, onChange } = data;
+  const { error, touched } = meta;
   return (
-    <input
-      className="box-border w-12 h-10 pl-2 mt-2 border-2 border-gray-300"
-      onChange={() => setValue()}
-      value={value}
-      type="number"
-    />
+    <div className="flex flex-col">
+      <label htmlFor={id} className="flex">
+        {label}
+        <GrEmergency className="mt-1 text-xs text-red-500" />
+      </label>
+      <input
+        id={id}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        name={name}
+        className="mt-2 placeholder-gray-500 border-2 "
+        {...rest}
+      />
+      {touched && error && <div className="text-red-600">{error}</div>}
+    </div>
   );
 }
 export default Input;
